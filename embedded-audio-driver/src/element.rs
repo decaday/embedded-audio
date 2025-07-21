4e87ab9bc3c0cfe7863a69ac0792a546a60be372
decaday <myDecaday@outlook.com>
2025-07-21 13:19:34 +0800

    Fix element traits and slot, rm unused

--- embedded-audio-driver/src/element.rs
+++ embedded-audio-driver/src/element.rs
@@ -1,23 +1,25 @@
+use embedded_io::{Read, Write, Seek};
+
 use crate::info::Info;
-use crate::TrySeek;
-use crate::Mutex;
+use crate::port::{InPort, OutPort, PortRequirement};
 
-use crate::slot::{InPort, OutPort};
-
+#[allow(async_fn_in_trait)]
 pub trait Element {
+    type Error;
+
     fn get_in_info(&self) -> Option<Info>;
 
     fn get_out_info(&self) -> Option<Info>;
 
+    fn get_in_port_requriement(&self) -> PortRequirement;
+
+    fn get_out_port_requriement(&self) -> PortRequirement;
+
     fn available(&self) -> u32;
 
-    async fn process(&mut self, in_port: Option<&InPort>, out_port: Option<&OutPort>) -> Result<(), ()>;
-}
+    async fn process<R, W>(&mut self, in_port: &mut InPort<'_, R>, out_port: &mut OutPort<'_, W>) -> Result<(), Self::Error>
+    where
+        R: Read + Seek,
+        W: Write + Seek;
 
-
-
-pub trait PassiveElement: Element {
-}
-
-pub trait NegativeElement: Element {
 }
--- embedded-audio/src/lib.rs
+++ embedded-audio/src/lib.rs
@@ -1,15 +1,22 @@
 pub mod encoder;
 pub mod decoder;
-pub mod stream;
-pub mod pipeline;
-pub mod generator;
-pub mod relay;
-pub mod utils;
-pub mod ringbuffer;
-pub mod transform;
+// pub mod stream;
+// pub mod pipeline;
+// pub mod generator;
+// pub mod utils;
+// pub mod ringbuffer;
+// pub mod transform;
 
 // use std::sync::Arc;
 // use embassy_time::{Duration, Timer};
 // use cpal::{Sample};
 // use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 // use ringbuffer::AllocRingBuffer as RingBuffer;
+
+cfg_if::cfg_if! {
+    if #[cfg(feature = "std")] {
+        pub use std::sync::Mutex;
+    } else {
+        pub type Mutex<T> = embassy_sync::blocking_mutex::CriticalSectionMutex<T>;
+    }
+}
